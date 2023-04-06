3240a54c83d761956810cc1e6bb18aed60b660ed
Patrick Freeman <patrick@csp-inc.org>
2023-04-06 15:55:43 -0700

    Update 05_bonusPts.R
    Clean up for calculation of Otero IBA stats

--- 05_bonusPts.R
+++ 05_bonusPts.R
@@ -1,53 +1,41 @@
 ##########################################
-## CALC AREAS IN MIGRATION ROUTES & IBA ##
+## CALC AREAS IN IBA ##
 ##########################################
 
 acre_in_km <- 247.105
 
-aoisShapes <- list(rd_allx, ls)
+aoisShapes <- list(otero)
 aoisNames <- c(
-  "Red Desert",
-  "Little Sandy"
+  "Otero Mesa"
 )
 
-# aoisShapes <- list(lewis)
-# aoisNames <- c(
-#   "Lewistown"
-# )
+a <- aoisShapes[[1]]; print(aoisNames[1])
 
+iba <- iba_nm
 
-# a <- aoisShapes[[1]]; print(aoisNames[1])
-a <- aoisShapes[[2]]; print(aoisNames[2])
-
-# iba <- iba_mt %>% as_Spatial()
-iba <- iba_wyo
-
-# Area in square m; nb can't set km in area()
-migrArea <- terra::area(migr) %>% sum()#/1000000
-ibaArea <- terra::area(iba) %>% sum()#/1000000
-
-# Get intersecting area; nb error may indicated ZERO overlap.
-migrAreaAoi <- st_as_sf(migr) %>%
-  st_intersection(st_as_sf(a)) %>%
-  as_Spatial() %>%
-  terra::area() %>%
-  sum()#/1000000
+# Calculate the overlap of the IBA with the proposed ACEC 
 ibaAreaAoi <- st_as_sf(iba) %>%
   st_intersection(st_as_sf(a)) %>%
   as_Spatial() %>%
   terra::area() %>%
   sum()#/1000000
 
-# Of all migration corridor or IBA area in the state, AOI has x %.
-(migrPercAoi <- migrAreaAoi/migrArea)
+# Of all IBA area in the state, AOI has x %.
+ibaArea <- iba %>% as_Spatial() %>% terra::area() %>% sum()#/1000000
 (ibaPercAoi <- ibaAreaAoi/ibaArea)
 
-(aoiArea <- a %>% terra::area() %>% sum())#/1000000
-(wyoArea <- wyo %>%  terra::area() %>% sum())#/1000000
-# (mtArea <- mt %>%  terra::area() %>% sum())#/1000000
+(ibaAreaAoi/1000000*acre_in_km)
 
-(aoiPercWyo <- aoiArea/wyoArea)
-# (aoiPercMt <- aoiArea/mtArea)
+### Overlap with the Otero Mesa IBA in particular 
+otero_iba <- iba_nm %>%
+  dplyr::filter(SITE_NAME == "Otero Mesa")
 
-(aoiArea/1000000*acre_in_km)
+otero_iba_area <- otero_iba %>% as_Spatial() %>% terra::area()
+otero_iba_AreaAoi <- otero_iba %>%
+  st_intersection(a) %>%
+  as_Spatial() %>%
+  terra::area() %>%
+  sum()
+
+(OteroibaPercAoi <- (otero_iba_AreaAoi/otero_iba_area))
 
